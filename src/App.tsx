177f8e56c5a2b71736a46bf649394b070a54e017
Daniel Flint <nauzilus@gmail.com>
2021-06-23 19:43:34 +1000

    feat: add unstoppable playlist

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import allDay from "./data/girl-talk_oh-no.json";
+import unstoppable from "./data/girl-talk_unstoppable.json";
 import { MashupBreakdown } from "./MashupBreakdown";
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
       <header className="App-header">
         <h1>Mashup Breakdown</h1>
       </header>
-      <MashupBreakdown data={allDay} />
+      <MashupBreakdown data={unstoppable} />
     </div>
   );
 }
